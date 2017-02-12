acdd4f48f9717eec64739ec14e29e91c7df4a2aa
Henrik Danielsson <henkedanne@gmail.com>
2017-02-12 22:49:18 +0100

    Got the api data

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 var posHolder = document.querySelector('#position');
 
+// api key = af7c0d1e9a573a42db1eb2be21afc6e0
+
+var wheaterData;
+var lat = 0;
+var long = 0;
+
+function getApi(url){
+	data = fetch(url)
+			.then(blob => blob.json())
+			.then(data => wheaterData.push(data)); //Hur får jag in data i wheaterData???
+			console.log(wheaterData);
+};
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(function (pos){
+        	
+        	
+			posHolder.innerHTML = pos.coords.latitude + "</br>" + pos.coords.longitude;
+			
+			var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude +"&lon=" + pos.coords.longitude + "&APPID=af7c0d1e9a573a42db1eb2be21afc6e0";
+
+        	getApi(url);
+        });
     } else {
         posHolder.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-function showPosition(position) {
-    posHolder.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
-}
+
 
 window.onload = getLocation();
+
+
