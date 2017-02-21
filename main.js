3d7b93e7be94e722769bb7fdc157c9b64ada6ba7
Henrik Danielsson <henkedanne@gmail.com>
2017-02-21 22:49:31 +0100

    placeholders in place and working as they should.

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 var posHolder = document.querySelector('#position');
 var tempHolder = document.querySelector('#temp');
 var weatherHolder = document.querySelector('#weather_data')
-// api key = c2ca76245286de363c961123ba8197b5
+var loader = document.getElementById('loader');
+var reloadHolder = document.querySelector('.reload');
 
 
 
 
 function getWeather(url){
-	fetch(url)
-			.then(function(blob) {
+	// fetching API data
+	fetch(url) 
+			.then(function(blob) { 
 				return blob.json();
 			})
 			.then(function(res){
-				console.log(res);
-				tempHolder.innerHTML = Math.floor(res.main.temp) + "°C";
+				// Displaying data in holders AND removing loadwheel
+				loader.classList.remove('loadwheel');
+				reloadHolder.style.display = "block";
+				tempHolder.innerHTML = Math.floor(res.main.temp) + "°";
 				weatherHolder.innerHTML = res.weather[0].description;
 				posHolder.innerHTML = "Near " + "<strong>" + res.name + "</strong>";
 			});
@@ -21,13 +25,18 @@ function getWeather(url){
 	};
 
 function getLocation() {
+	// clearing all the elements
+	posHolder.innerHTML = "";
+	tempHolder.innerHTML = "";
+	weatherHolder.innerHTML = "";
+	// Displaying the loadwheel AND removing "reload"
+	reloadHolder.style.display = "none";
+	loader.classList.add('loadwheel');
+    // Getting location
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(pos){
         	
-        	
-			//posHolder.innerHTML = pos.coords.latitude + "</br>" + pos.coords.longitude;
-			
-			var url = "https://cors.now.sh/" + "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude +"&lon=" + pos.coords.longitude + "&units=metric" +"&APPID=c2ca76245286de363c961123ba8197b5";
+        	var url = "https://cors.now.sh/" + "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude +"&lon=" + pos.coords.longitude + "&units=metric" +"&APPID=c2ca76245286de363c961123ba8197b5";
 
         	getWeather(url);
         });
@@ -35,9 +44,9 @@ function getLocation() {
         posHolder.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-
-
-
+// Reload on click
+reloadHolder.addEventListener('click', getLocation);
+// Loads on start
 window.onload = getLocation();
 
 
