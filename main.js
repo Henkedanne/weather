345cd1f5b2bb7b546280f8d27f44ed9689b75e1b
Henrik Danielsson <henkedanne@gmail.com>
2017-03-23 15:08:19 +0100

    added forcast
    added Forecast and styles for it

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var posHolder = document.querySelector('#position');
 var tempHolder = document.querySelector('#temp');
-var weatherHolder = document.querySelector('#weather_data')
+var weatherHolder = document.querySelector('#weather_data');
 var reloadHolder = document.querySelector('.reload i');
 var reloadClick = document.querySelector('.reload');
-var iconHolder = document.querySelector('#weather_icon')
-
+var iconHolder = document.querySelector('#weather_icon');
+var forTempHolder = document.querySelector('#fore-temp');
+var forHolder = document.querySelector('#fore-data');
 
 
 function getWeather(url){
@@ -13,10 +14,10 @@ function getWeather(url){
 			.then(function(blob) { 
 				return blob.json();
 			})
-			.then(function(res){
+			.then(function(result){
 				
 				var iconType = "";
-				var weatherId = res.weather[0].id;
+				var weatherId = result.weather[0].id;
 				console.log(weatherId);
 
 				if (weatherId >= 200 && weatherId <= 232 ){
@@ -39,12 +40,23 @@ function getWeather(url){
 				iconHolder.style.display = "block";
 				iconHolder.classList.add(iconType);
 				reloadHolder.classList.remove('fa-spin');
-				tempHolder.innerHTML = Math.floor(res.main.temp) + "°";
-				weatherHolder.innerHTML = res.weather[0].description;
-				posHolder.innerHTML = "Near " + "<strong>" + res.name + "</strong>";
+				tempHolder.innerHTML = Math.floor(result.main.temp) + "°";
+				weatherHolder.innerHTML = result.weather[0].description;
+				posHolder.innerHTML = "Near " + "<strong>" + result.name + "</strong>";
 			});
 			
 	};
+function getForecast(url) {
+	fetch(url)
+				.then(function(blob) {
+					return blob.json();
+				})	
+				.then(function(result) {
+					console.log(result);
+					forTempHolder.innerHTML = "Max: " + Math.floor(result.list[7].main.temp_max) + "°" + "<br>" + "Min: " + Math.floor(result.list[7].main.temp_min) + "°";
+					forHolder.innerHTML = result.list[7].weather[0].description;
+				});
+};
 
 function getLocation() {
 	// clearing all the elements
@@ -59,9 +71,10 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(pos){
         	
-        	var url = "https://cors.now.sh/" + "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude +"&lon=" + pos.coords.longitude + "&units=metric" +"&APPID=c2ca76245286de363c961123ba8197b5";
-
-        	getWeather(url);
+        	var urlCurrent = "https://cors.now.sh/" + "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude +"&lon=" + pos.coords.longitude + "&units=metric" +"&APPID=c2ca76245286de363c961123ba8197b5";
+        	var urlForecast = "https://cors.now.sh/" + "http://api.openweathermap.org/data/2.5/forecast?lat=" + pos.coords.latitude +"&lon=" + pos.coords.longitude + "&units=metric" +"&APPID=c2ca76245286de363c961123ba8197b5";
+        	getWeather(urlCurrent);
+        	getForecast(urlForecast);
         });
     } else {
         posHolder.innerHTML = "Geolocation is not supported by this browser.";
